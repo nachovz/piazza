b53823201c28704f07e9f6152c855ca8b3f485b2
Nacho <theignaciocordoba@gmail.com>
2023-11-03 18:41:28 -0400

    feat: add users controller test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -5,4 +5,42 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     get sign_up_path
     assert_response :success
   end
+
+  test "redirects to feed after successful sign up" do
+    get sign_up_path
+    assert_response :ok
+
+    assert_difference ["User.count", "Organization.count"], 1 do
+      post sign_up_path, params: {
+        user: {
+          name: "John",
+          email: "john@example.com",
+          password: "password",
+        }
+      }
+    end
+
+    assert_redirected_to root_path
+    follow_redirect!
+    assert_select ".notification.is-success", text: I18n.t("users.create.welcome", name: "John")
+  end
+
+  test "renders errors if input data is invalid" do
+    get sign_up_path
+    assert_response :ok
+
+    assert_no_difference ["User.count", "Organization.count"] do
+      post sign_up_path, params: {
+        user: {
+          name: "John",
+          email: "john@example.com",
+          password: "passd",
+        }
+      }
+    end
+
+    assert_response :unprocessable_entity
+    assert_select "p.is-danger", text: I18n.t("activerecord.errors.models.user.attributes.password.too_short")
+  end
+
 end
