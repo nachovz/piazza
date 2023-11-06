daceb89d2d1d3652a78234cf46b8ffed679ee8af
Nacho <theignaciocordoba@gmail.com>
2023-11-06 14:51:59 -0500

    feat: setting encrypted cookie

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,8 @@ class UsersController < ApplicationController
 
     if @user.save
       @organization = Organization.create(members: [@user])
-      # TODO: Log in user...
+      @app_session = @user.app_sessions.create
+      log_in(@app_session)
 
       redirect_to root_path, status: :see_other, flash: {success: t(".welcome", name: @user.name)}
     else
@@ -17,7 +18,8 @@ class UsersController < ApplicationController
   end
 
   private
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation	)
-    end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -21,6 +21,8 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     end
 
     assert_redirected_to root_path
+    assert_not_empty cookies[:app_session]
+
     follow_redirect!
     assert_select ".notification.is-success", text: I18n.t("users.create.welcome", name: "John")
   end
@@ -42,5 +44,4 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :unprocessable_entity
     assert_select "p.is-danger", text: I18n.t("activerecord.errors.models.user.attributes.password.too_short")
   end
-
 end
